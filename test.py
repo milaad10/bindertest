db122524c691be7b0fc3a6cb28c3ef65f66bb12a
milaad10 <86025294+milaad10@users.noreply.github.com>
2022-12-30 00:22:35 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-print('type:')
+import vk_api
 
-Index = input()
+def captcha_handler(captcha):
+  print("Enter captcha code " + captcha.get_url())
+#   return captcha.try_again(key)
 
-print('index=' + Index)
+vk_session = vk_api.VkApi('instagram.man1373@gmail.com', '(11a12b13c14d15e16F)', captcha_handler = captcha_handler)
+vk_session.auth()
+vk = vk_session.get_api()
+# try:
+
+# except vk_api.AuthError as error_msg:
+#   print(error_msg)
